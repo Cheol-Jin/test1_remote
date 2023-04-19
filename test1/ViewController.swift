aa1a9ecad97533866c0e4c6ad4ba0414b162b886
Cheol-Jin <cil6879@gmail.com>
2023-04-19 22:38:55 +0900

    test1

--- test1/ViewController.swift
+++ test1/ViewController.swift
@@ -8,10 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //제목
+    var titlelabel: UILabel = {
+        let label = UILabel()
+        label.text = "헬로우 월드!!"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 50)
+        label.textColor = .white
+        
+        return label
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+        view.addSubview(titlelabel)
+        
+        titlelabel.translatesAutoresizingMaskIntoConstraints = false
+        titlelabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true //x축 정가운데 둠
+        titlelabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true //y축 정가운데 둠
+        
     }
 
 
